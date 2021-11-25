e49090c467984a5f163111fd769861b4b5a5d4db
kurian <kurianck.mail@gmail.com>
2021-11-26 00:16:08 +0530

    python string format

--- space_pic.py
+++ space_pic.py
@@ -13,16 +13,17 @@ def test_api():
     return True
 
 def get_pic_of_day():
-    today = datetime.today()
+    today = datetime.today()    
     start_date = today - timedelta(days=1)
     start_date = start_date.strftime("%Y-%m-%d")
     end_date = today.strftime("%Y-%m-%d")
-    res = requests.get("https://api.nasa.gov/planetary/apod?&start_date={}&end_date={}&api_key={}".format(start_date,end_date,API_KEY))
+    res = requests.get(f"https://api.nasa.gov/planetary/apod?&start_date={start_date}&end_date={end_date}&api_key={API_KEY}")
     return res
 
 if __name__ == "__main__":
     for pic in get_pic_of_day().json():
         res = requests.get(pic["url"])
         ext = pic["url"].split(".")[-1]
-        with open(pic["title"]+"."+ext,"wb+") as imagefile:
+        file_name = pic["title"] 
+        with open(f"{file_name}.{ext}","wb+") as imagefile:
             imagefile.write(res.content)
